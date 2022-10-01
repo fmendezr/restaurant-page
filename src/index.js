280302b68bbc23421076f4406845b5347a1d59bc
Felix Mendez <mendez.felixrn@gmail.com>
2022-10-01 19:35:13 -0400

    Make main for homepage

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import "./style.css";
+import profilePic from "./Hattori_Hanzo.jpeg"
 
 const componentHeader = () => {
 
@@ -34,4 +35,38 @@ const componentHeader = () => {
     contactBtn.classList.add("navBtn");
 }
 
+const componentMainHomePage = () => {
+
+     // fetch container
+     const container =  document.getElementById("content");
+
+    // create elements 
+    const main = document.createElement("main");
+    const text1 = document.createElement("p");
+    const text2 = document.createElement("p");
+    const img = new Image();
+    img.src = profilePic;
+    const text3 = document.createElement("p");
+
+    // insert element into HTML
+    container.appendChild(main);
+    main.appendChild(text1);
+    main.appendChild(text2);
+    main.appendChild(img);
+    main.appendChild(text3);
+
+    // add text content 
+    text1.textContent = "Demon Hanzō's cuisine ";
+    text2.textContent = "In  Edo since 1592";
+    text3.textContent = '"Wake up from death and return to life"';
+
+    // add style 
+    img.setAttribute("id", "profilePic");
+    text1.classList.add("txt");
+    text2.classList.add("txt");
+    text3.classList.add("txt");
+}
+
+
 componentHeader();
+componentMainHomePage();
