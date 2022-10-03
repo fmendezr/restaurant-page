d3d92f238edcb27c04378a80c314c8ed2674bdd6
Felix Mendez <mendez.felixrn@gmail.com>
2022-10-02 20:09:46 -0400

    Make and style contact main component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import "./style.css";
 import componentHeader from "./header.js";
-import {componentHome, componentMenu} from "./mainElements.js";
+import {componentHome, componentMenu, componentContact} from "./mainElements.js";
 import componentFooter from "./footer.js";
 
 componentHeader();
-componentMenu();
+componentContact();
 componentFooter();
--- src/mainElements.js
+++ src/mainElements.js
@@ -7,6 +7,9 @@ import pufferFishImg from "./img/menu/pufferFish.svg";
 import ramenImg from "./img/menu/ramen.svg";
 import riceBowlImg  from "./img/menu/riceBowl.svg";
 import sushiRollImg from "./img/menu/sushiRoll.svg";
+import houseImg from "./img/contact/house.svg";
+import phoneImg from "./img/contact/phone.svg";
+import mapImg from "./img/contact/map.jpeg";
 
  const componentHome = () => {
 
@@ -39,8 +42,6 @@ import sushiRollImg from "./img/menu/sushiRoll.svg";
    txt1.classList.add("txt");
    txt2.classList.add("txt");
    txt3.classList.add("txt");
-
-   console.log("it's working");
 }
 
 const componentMenu = () => {
@@ -48,6 +49,7 @@ const componentMenu = () => {
     //fetch container 
     const container = document.getElementById("content");
     
+    // set up main
     const main = document.createElement("main");
     container.appendChild(main)
     main.classList.add("menu");
@@ -80,9 +82,53 @@ const componentMenu = () => {
         description.classList.add("foodDescription");
         description.id = `desc${i}`;        
     }
-
-
-    console.log("Do you know the story of the scorpion and the frog?")
 }
 
-export {componentHome, componentMenu};
+const componentContact = () => {
+
+    //fetch container 
+    const container = document.getElementById("content");
+
+    // set up main
+    const main = document.createElement("main");
+    container.appendChild(main)
+    main.classList.add("contact");
+
+    // create elements 
+    const phoneDiv = document.createElement("div");
+    const phoneNumber = document.createElement("p");
+    const phoneIconImg = new Image();
+    phoneIconImg.src = phoneImg;
+    const directionDiv = document.createElement("div");
+    const direction = document.createElement("p");
+    const houseIconImg = new Image()
+    houseIconImg.src = houseImg;
+    const imgMap = new Image();
+    imgMap.src = mapImg;
+
+    // insert element into DOM 
+    main.appendChild(phoneDiv);
+    phoneDiv.appendChild(phoneIconImg);
+    phoneDiv.appendChild(phoneNumber);
+    main.appendChild(directionDiv);
+    directionDiv.appendChild(houseIconImg);
+    directionDiv.appendChild(direction);
+    main.appendChild(imgMap);
+
+    // add text content 
+    phoneNumber.textContent = "+81 03-3224-9999";
+    direction.textContent = "Edo"
+
+    // add attributes
+    phoneIconImg.classList.add("icon");
+    houseIconImg.classList.add("icon");
+    phoneDiv.id = "phoneDiv";
+    directionDiv.id = "directionDiv";
+    phoneNumber.id = "phoneNumber";
+    direction.id = "direction";
+    imgMap.id = "map";
+
+    console.log("Do you know the story of the scorpion and the frog?");
+}
+
+export {componentHome, componentMenu, componentContact};
